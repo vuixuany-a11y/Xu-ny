e45a62eb89f2df24e3425e7f81cf269aeac2c10a
vuixuany-a11y <vuixuany@gmail.com>
2025-12-12 16:10:20 +0700

    Update app.py

--- app.py
+++ app.py
@@ -347,4 +347,4 @@ if st.button("🚀 SOẠN GIÁO ÁN NGAY"):
 
 # --- CHÂN TRANG ---
 st.markdown("---")
-st.markdown("<div style='text-align: center; color: #666;'>© 2025 - Sùng Văn Quý - Trường PTDTBT Tiểu học Đường Thượng - ĐT: 097 4109 364</div>", unsafe_allow_html=True)
+st.markdown("<div style='text-align: center; color: #666;'>© 2025 - Vùi Xuân Ý - Trường PTDTBT Tiểu học Mạu Long - ĐT: 0962830703</div>", unsafe_allow_html=True)
