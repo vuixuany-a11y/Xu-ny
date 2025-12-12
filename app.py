848a57769061a56e724a587a156cc2044fce4681
vuixuany-a11y <vuixuany@gmail.com>
2025-12-12 15:51:36 +0700

    Update app.py

--- app.py
+++ app.py
@@ -191,7 +191,7 @@ st.markdown("""
 st.markdown("""
 <div class="main-header">
     <h1>📘 TRỢ LÝ SOẠN GIÁO ÁN TỰ ĐỘNG (NLS)</h1>
-    <p>Tác giả: Sùng Văn Quý - Trường PTDTBT Tiểu học Đường Thượng - ĐT: 097 4109 364</p>
+    <p>Tác giả: Vùi Xuân Ý - Trường PTDTBT Tiểu học Mạu Long - ĐT: 0962830703</p>
 </div>
 """, unsafe_allow_html=True)
 
